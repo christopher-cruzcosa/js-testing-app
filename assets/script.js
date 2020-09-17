9f4f9d47d324a7f925aac114e1180cdb7cce5db0
Chris C <christopher.cruzcosa@gmail.com>
2020-09-16 23:21:16 -0700

    Added notations to the js file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
+
+//This sets the main element locations that remain persistent on the app regardless of later actions
 var mainContentLoc = document.getElementById("mainContent");
 var mainButtonLoc = document.getElementById("mainButton");
 var highScoresButtonLoc = document.getElementById("viewHighScores");
 
+//These variables control the quiz question progression and the timer in seconds
 var sectionCount = 0;
 var timerSeconds = 100;
+
+//These variables are where the user's final score and initials will go; also, this is the array that holds all of the score/initial objects
 var initials = "" ;
 var score = 0;
 var highScores = [];
 
+//this function re-sorts the highScores
 function compareValues(key, order = 'asc') {
     return function innerSort(a, b) {
       if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
@@ -33,9 +39,7 @@ function compareValues(key, order = 'asc') {
   };
 
 
-
-
-
+//this function checks if there are any high scores in local storage and, if yes, addes them to the high score array above
 function init() {
   var existingScores = JSON.parse(localStorage.getItem("highScoreList"));
 
@@ -45,10 +49,10 @@ function init() {
   console.log(highScores);
 }
 
-
-
 init();
 
+
+//This function begins the timer, subtracts a second each second, displays the timer, and then shuts it down when the time is zero or the fifth question has been answered
 function setTime() {
     var timerInterval = setInterval(function() {
       timerSeconds--;
@@ -67,14 +71,19 @@ function setTime() {
     }, 1000);
 };
 
+
+//this variable controls the correct/wrong line that will appear beneath each new question, saying if the past question was right or wrong
 var correctamundo = "";
 
+//this array holds all of the questions
 var questions = ["Question 1 of 5: Inside which HTML element do we put the JavaScript?",
 `Question 2 of 5: How do you write "Hello World" in an alert box?`,
 "Question 3 of 5: Where is the correct place to insert a JavaScript?",
 "Question 4 of 5: What is the correct syntax for referring to an external script called 'xxx.js'?",
 'Question 5 of 5: How do you call a function named "myFunction"?']
 
+
+//these arrays holds the options for each question, and the final entry in the array is the index of the correct answer
 var choices0 = ["<scripting>","<script>","<js>","<javascript>",1];
 var choices1 = [`msgBox("Hello World");`,`msg("Hello World");`,
 `alertBox("Hello World");`,`alert("Hello World");`,3]
@@ -85,13 +94,17 @@ var choices3 = [`<script src="xxx.js">`,
 var choices4 = [`call function myFunction()`,
 `call myFunction()`,`myFunction()`,"None of the above",2]
 
+//this is the click that is listening for the main button on the main page, and starts the timer
 mainButtonLoc.addEventListener("click",function() {
     setTime();
 })
 
+//this is the event listener that controls all of the quiz answers and the question progression, 
 mainContentLoc.addEventListener("click", function(event){
     event.preventDefault();
     event.stopPropagation();
+
+    //these are the actions that generate the first quiz question, including options and formating
     if (sectionCount === 0) {
         mainContentLoc.innerHTML = "";
         var node = document.createElement("H2");
@@ -112,6 +125,7 @@ mainContentLoc.addEventListener("click", function(event){
         };
     };
 
+    //these are the actions that generate the second quiz question, including options and formating
     if (sectionCount === 1) {
         if (eval("choices" + (sectionCount-1)).indexOf(event.target.textContent) != choices0[4]) {
             timerSeconds -= 10;
@@ -152,6 +166,7 @@ mainContentLoc.addEventListener("click", function(event){
             return};
     };
 
+    //these are the actions that generate the third quiz question, including options and formating
     if (sectionCount === 2) {
         if (choices1.indexOf(event.target.textContent) != choices1[4]) {
             timerSeconds -= 10;
@@ -192,6 +207,7 @@ mainContentLoc.addEventListener("click", function(event){
             return};
     };
 
+    //these are the actions that generate the fourth quiz question, including options and formating
     if (sectionCount === 3) {
         if (choices2.indexOf(event.target.textContent) != choices2[4]) {
             timerSeconds -= 10;
@@ -232,6 +248,7 @@ mainContentLoc.addEventListener("click", function(event){
             return};
     };
 
+    //these are the actions that generate the fifth quiz question, including options and formating
     if (sectionCount === 4) {
         if (choices3.indexOf(event.target.textContent) != choices3[4]) {
             timerSeconds -= 10;
@@ -272,6 +289,7 @@ mainContentLoc.addEventListener("click", function(event){
             return};
     };
 
+    //these are the actions that generate the end screen, that sets the new score, displays the score, and gives the user an input field to put their initials and then submit
     if (sectionCount === 5) {
         if (choices4.indexOf(event.target.textContent) != choices4[4]) {
             timerSeconds -= 10;
@@ -281,7 +299,6 @@ mainContentLoc.addEventListener("click", function(event){
             correctamundo = true;
             score = timerSeconds;
         };
-
         mainContentLoc.innerHTML = "";
         var node = document.createElement("H2");
         mainContentLoc.appendChild(node);
@@ -307,6 +324,7 @@ mainContentLoc.addEventListener("click", function(event){
         buttonE3.id = "buttonFinal";
     };
 
+    //these are the actions that happen after the initiasl have been submitted, which generates the high scores screen
     if (event.target.id === "buttonFinal") {
         initials = document.getElementById("initialsField").value;
         var newScore = {
@@ -337,16 +355,18 @@ mainContentLoc.addEventListener("click", function(event){
         node.appendChild(highScoreButton);
     };
 
+    //when the user clicks "done" this reloads the main screen
     if (event.target.id === "goAwayFromScoresButton") {
         location.reload();
     };
 
+
+    // each time a user clicks any option, it increases the sectionCount, which enables the question/screeen progression
     sectionCount++;
 });
 
-
+//When the user clicks the high scores button on the navbar, this brings up the high scores screen 
 highScoresButtonLoc.addEventListener("click", function(event){
-
     event.preventDefault();
     event.stopPropagation();
     mainContentLoc.innerHTML = "";
@@ -371,6 +391,7 @@ highScoresButtonLoc.addEventListener("click", function(event){
         node.appendChild(highScoreButton);
 });
 
+//when the user goes to the high scores screen from the main screen, after they click done, this takes them back to the main screen
 document.getElementById("goAwayFromScoresButton").addEventListener("click", function(event){
     event.preventDefault();
     event.stopPropagation();
